22654a3fd4c32edca4d8d8e826d1abc342535304
luizdefranca <luizramospe@hotmail.com>
2016-08-11 15:24:32 -0300

    Initial Commit

---
+++ projetoAlura01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projetoAlura01
+//
+//  Created by LuizRamos on 11/08/16.
+//  Copyright © 2016 LuizRamos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
