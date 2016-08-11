29b69a31a0fa25cf2b1d770e7b33f3c7abbe06d2
luizdefranca <luizramospe@hotmail.com>
2016-08-11 16:34:29 -0300

    Final da lição 01

--- projetoAlura01/ViewController.swift
+++ projetoAlura01/ViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+   
+    @IBOutlet var nameTxtField: UITextField!
+    
+    @IBOutlet var happinessTxtField: UITextView!
+    
+    
+    
+    @IBAction
+    func add(){
+        let name = nameTxtField.text
+        let happiness = happinessTxtField.text
+        print("eaten: \(name)  \(happiness)")
     }
 
 
